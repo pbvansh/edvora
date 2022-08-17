ca512997c9dec076ec404a7f0b0b9efc6acd9a33
pbvansh <vanshpratik0165@gmail.com>
2022-08-18 01:10:45 +0530

    create welcome page

--- pages/userFeed.js
+++ pages/userFeed.js
@@ -11,7 +11,7 @@ const userFeed = ({users}) => {
             users.map(({name,user_id})=>(
                 <div key={user_id} className='bg-gray-100 group m-5 flex flex-col items-center cursor-pointer rounded-sm shadow-md md:hover:scale-125 transition delay-100 ease-in-out duration-500 md:transform-none'>
                     <div className="m-5">
-                    <Image className="rounded-full m-10 group-hover:animate-pulse" height={100} width={100} src={`https://randomuser.me/api/portraits/men/${user_id}.jpg`} />
+                    <Image className="rounded-full m-10 group-hover:animate-pulse" height={100} width={100} src={`https://randomuser.me/api/portraits/men/${user_id}.jpg`} alt='User Image'/>
                     </div>
                     <p className="m-5 font-bold group-hover:animate-bounce text-red-500">{name}</p>
                 </div>
